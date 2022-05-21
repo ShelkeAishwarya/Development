72e052ea56310db6a5e76b6a515c82ef0ffb4823
ShelkeAishwarya <105958011+ShelkeAishwarya@users.noreply.github.com>
2022-05-21 08:30:34 +0530

    Add files via upload

---
+++ program6.c
@@ -0,0 +1,21 @@
+//Display 5 times Hello on screen 
+
+
+#include<stdio.h>
+
+void Display();
+
+int main()
+{
+Display();
+return 0;
+}
+
+void Display()
+{
+printf("Hello... \n");
+printf("Hello... \n");
+printf("Hello... \n");
+printf("Hello... \n");
+printf("Hello... \n");
+}
